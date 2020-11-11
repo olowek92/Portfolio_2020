48426dc0d771d9e2135aca325b4cf8df8b6fdffd
Wojciech <olowek92@gmail.com>
2020-11-11 14:11:02 +0100

    Updates

---
+++ precache-manifest.9172a01694efd4800b17e26351fbe49f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a382703cc21ced6f4c810de2cbca339d",
+    "url": "/Portfolio_2020/index.html"
+  },
+  {
+    "revision": "dd163e9945ccdf434572",
+    "url": "/Portfolio_2020/static/js/2.9f200eee.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/Portfolio_2020/static/js/2.9f200eee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43c37699a5be0acd453c",
+    "url": "/Portfolio_2020/static/js/main.f510c350.chunk.js"
+  },
+  {
+    "revision": "efb06d04641d1e59bb06",
+    "url": "/Portfolio_2020/static/js/runtime-main.6f25824a.js"
+  }
+]);
