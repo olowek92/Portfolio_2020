d511bff47aeb3497bef6931751e5ce94cb636dc6
Wojciech <olowek92@gmail.com>
2020-11-16 19:16:41 +0100

    Updates

---
+++ precache-manifest.decedf4d3cacb8fa3ff6b621417c778e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca64a7e4673f18bd01a0751660b43d2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15e0256bf312ea43b382",
+    "url": "/static/js/2.81af4f55.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/static/js/2.81af4f55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "207a72b1cb05f5b3600b",
+    "url": "/static/js/main.02f4b235.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
